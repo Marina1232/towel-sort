ca69f03faf094ca2adbfc6ff3cda88cc7c241cd4
Marina1232 <marina.saiko.18@gmail.com>
2022-07-26 16:11:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+const isOddNumber = (index) => {
+    return index === 1 || index % 2 === 1;
+};
+
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix)) {
+        return [];
+    }
+    if (matrix.length === 0) {
+        return [];
+    }
+
+    matrix.forEach((item, index) => {
+        if (isOddNumber(index)) {
+            item.reverse();
+        }
+    });
+
+    const newArray = matrix.join(",").split(",");
+    const array = newArray.map((item) => {
+        return parseInt(item);
+    });
+    // if (matrix.length === 0 || arguments.length === 0) {
+    //     return [];
+    // }
+    return array;
+};
